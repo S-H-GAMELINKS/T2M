3c5d7d8e6f10497d7e4968edc922cc1d88f56eb6
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-24 22:55:28 +0900

    add find methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.find_for_oauth(auth)
+    user = User.where(provider: auth.provider, email: User.dummy_email(auth)).first
+
+    if user != nil then
+      user.update!(uid: auth.extra.raw_info.display_name)
+    end
+
+    unless user
+      user = User.create(
+        uid:      auth.extra.raw_info.display_name,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20],
+        token: auth.credentials.token.to_s
+      )
+      user.save!
+    end
+    current_user = user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}"
+  end
 end
