dd8b8e33d69164539056821734c5a53e314155a6
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-25 12:26:18 +0900

    can toot to mastodon

--- app/controllers/t2_m_controller.rb
+++ app/controllers/t2_m_controller.rb
@@ -1,4 +1,20 @@
 class T2MController < ApplicationController
   def index
   end
+
+  def import
+    require 'csv'
+
+    current_user.email.match("@")
+
+    client = Mastodon::REST::Client.new(base_url: "https://" + $', bearer_token: current_user.token)
+
+    toot = String.new
+
+        CSV.foreach(params[:file].path, headers: true) do |row|
+          toot = row.to_hash.slice("text")
+          puts toot["text"]
+          response = client.create_status(toot["text"])
+        end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,12 +260,12 @@ Devise.setup do |config|
 
   config.omniauth :mastodon, ENV['ACCESS_KEY'], ENV['SECRET_KEY']
 
-  config.omniauth :mastodon, scope: 'read follow', credentials: lambda { |domain, callback_url|
+  config.omniauth :mastodon, scope: 'read write follow', credentials: lambda { |domain, callback_url|
   client = MastodonClient.where(domain: domain).first_or_initialize(domain: domain)
 
   return [client.client_id, client.client_secret] unless client.new_record?
 
-  new_client = Mastodon::REST::Client.new(base_url: "https://#{domain}").create_app('MastodonLoginSample', callback_url, 'read follow')
+  new_client = Mastodon::REST::Client.new(base_url: "https://#{domain}").create_app('MastodonLoginSample', callback_url, 'read write follow')
 
   client.client_id = new_client.client_id
   client.client_secret = new_client.client_secret
