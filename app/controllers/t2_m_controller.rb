364f58d37cc7ee05f6013723934f187d953482bf
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-29 16:16:50 +0900

    bundle update #1

--- app/controllers/t2_m_controller.rb
+++ app/controllers/t2_m_controller.rb
@@ -1,4 +1,16 @@
 class T2MController < ApplicationController
   def index
   end
+
+  def import
+
+    client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["ACCESS_TOKEN"])
+
+    require 'csv'
+    CSV.foreach(params[:file].path, headers: true) do |row|
+      toot = new
+      toot = row.to_hash.slice(["text"])
+      response = client.create_status(toot)
+    end
+  end
 end
